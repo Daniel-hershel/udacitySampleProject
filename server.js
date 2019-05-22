b2b6b3e9fa7f5b6eef68e2245b0e68b6df5237d8
hershel <daniel.h.silber.baker@gmail.com>
2019-05-22 13:15:30 -0700

    refactored and added comments for server.js

--- server.js
+++ server.js
@@ -1,40 +1,38 @@
+// Setup empty JS object to act as endpoint for all routes
 testData = {
 
 }
-const fs = require('fs');
-const cpuData = fs.readFileSync('externalData.json');
-// const data = JSON.parse(cpuData)
-// console.log(data)
-//express to run server
-const express = require('express');
-const axios = require('axios');
-//cors for cross origin aloowance
-var cors = require('cors');
 
-//start up an instance of app. This needs to go before any of the things below which are attached to the app
+// Express to run server
+const express = require('express');
+
+// Start up an instance of app. 
 const app = express();
+
+// Cors for cross origin aloowance
+var cors = require('cors');
 app.use(cors());
 
+// Initialize the main project folder
+app.use(express.static('website'))
+
+// Spin up the server
 const server = app.listen(3000, listening)
-//callback to debug
+// Callback to debug
 function listening(){
   console.log('listening');
     }
-app.use(express.static('website'))
 
-// app.get('/add/:temp/:feel', addEntry)
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-
+// Initialize all route with a callback function
 app.get('/all', sendData)
 
+// Callback function to complete GET '/all'
 function sendData (req, res) {
   // res.send('Working')
   res.send(testData)
 }
 
+// Test for adding data from the Server side
 app.get('/add/:temp/:feel', addEntry)
 function addEntry(request, response){
   let data = request.params;
@@ -48,3 +46,16 @@ function addEntry(request, response){
 
   console.log(testData)
 }
+
+
+
+
+// Depreciated solution of using an external JSON file as route endpoint
+// const fs = require('fs');
+// const cpuData = fs.readFileSync('externalData.json');
+// const data = JSON.parse(cpuData)
+// console.log(data)
+// Depreciated external JSON solution cont...
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.json()); // support json encoded bodies
+// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
