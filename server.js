4ed57c6dbade2b4860cee838ff737064b402079b
hershel <daniel.h.silber.baker@gmail.com>
2019-05-27 11:08:15 -0700

    clean up server code

--- server.js
+++ server.js
@@ -8,19 +8,18 @@ const express = require('express');
 // Start up an instance of app
 const app = express();
 
+/* Dependencies */
 const bodyParser = require('body-parser')
-const http = require('http')
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
-
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // Spin up the server
 const server = app.listen(3000, listening);
@@ -29,12 +28,6 @@ function listening(){
     // console.log(server);
     console.log('on');
   };
-  
-// Do I need a second server running
-// http.createServer(function(req,res){
-//   res.write('Hello World!'); //write a response to the client
-//   res.end(); //end the response
-// }).listen(8080); //the server object listens on port 8080
 
 // Initialize all route with a callback function
 app.get('/all', sendData);
@@ -47,13 +40,9 @@ function sendData (request, response) {
 
 app.post('/add', (request,response)=>{
   // console.log(request.body)
-  // add to projectData
   let data = request.body;
   console.log(data.temp);
 
-  // let temp = data.temp;
-  // let feel= data.feel;
-  // let date= data.date;
   // Create new entry for JS Object Endpoint
   projectData["temp"] = data.temp;
   projectData["feel"] = data.feeling;
@@ -61,5 +50,4 @@ app.post('/add', (request,response)=>{
 
   // Send response to Endpoint
   response.send(projectData);
-  // res.send('updated')
 })
