dd7d434ebdfa83618f6e013677b3227f2ce178a8
hershel <daniel.h.silber.baker@gmail.com>
2019-05-27 14:43:53 -0700

    refactoring functions to be async and use await

--- website/app.js
+++ website/app.js
@@ -5,7 +5,7 @@ const apiKey = '&appid=9f15e45060210ec849a698b3298f0bed&units=imperial';
 document.getElementById('generate').addEventListener('click', performAction);
 
 /* Function called by event listener */
-function performAction(e) {
+ function performAction(e) {
   // Retrieve the zipcode entered by the user via the DOM input element with the ID 'zip'
   let userZip = document.getElementById('zip').value;
   // Use the fetch API to retrieve the current weather data for the users zip code
@@ -21,18 +21,19 @@ function performAction(e) {
     // Create a new date entry for JS object
     let d = new Date();
     let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-    // Use a chained fetch to access the 'add' route created in the server
-    // And create a new entry using the URL structure and the variables retrieved above
+    // Call function to POST data with params
     postData('/add', {temp:temp, feeling:feeling, date:newDate})
+    // Call function to Get data and update UI
+    retrieveData()
 
     }).catch(function(error) {
       console.log('There has been a problem with your fetch operation: ', error.message);
     });
 
 /* Function to POST data */
-function postData( url = '', data = {}){
+async function postData( url = '', data = {}){
   console.log(data)
-    return fetch('/add', {
+    const response = await fetch(url, {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
     // mode: 'cors', // no-cors, cors, *same-origin
     // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -42,26 +43,34 @@ function postData( url = '', data = {}){
         // 'Content-Type': 'application/x-www-form-urlencoded',
     },
     body: JSON.stringify(data), // body data type must match "Content-Type" header        
-})
-.then(function(response){
-  return response.json()
-}).then(function(body){
-  console.log(body);
-});
+  });
+
+    try {
+      const newData = await response.json();
+      // console.log(newData);
+      return newData
+    }catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+    }
 }
 
-/* GET Project Data */
-fetch('/all')
-// Transform into JSON
-.then((resp) => resp.json())
-.then(function(allData){
+/* Function to GET Project Data */
+async function retrieveData () { 
+  const request = await fetch('/all');
+  try {
+  // Transform into JSON
+  const allData = await request.json()
   console.log(allData)
   // Write updated data to DOM elements
   document.getElementById('temp').innerHTML = Math.round(allData.temp)+ 'degrees';
   document.getElementById('content').innerHTML = allData.feel;
   document.getElementById("date").innerHTML =allData.date;
-}).catch(function(error) {
-  console.log('There has been a problem with your fetch operation: ', error.message);
-});
+  }
+  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
 
 }
