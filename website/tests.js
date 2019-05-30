61d7a2127092b5c187861277d3eef4061bdacc4e
hershel <daniel.h.silber.baker@gmail.com>
2019-05-30 15:34:42 -0700

    adding semicolons for good practice

--- website/tests.js
+++ website/tests.js
@@ -8,11 +8,11 @@ const testApi = async (url= '', apiKey= '')=>{
     .then((resp) => resp.json())
     // Function to execute once data has successfully been retrieved as JSON-- Creating a new the pieces for a new entry in our JS object endpoint
     .then(function(data){
-        console.log(data)
+        console.log(data);
     })
-}
+};
 
-testApi(testUrl, testKey)
+testApi(testUrl, testKey);
 
 
 /* POST Test */
@@ -27,26 +27,26 @@ const postTest = async ( url= '', data = {})=>{
 
     try {
         const newData = await response.json()
-        console.log(newData)
-        return newData
+        console.log(newData);
+        return newData;
     }catch(error){
-        console.log('error', error)
-    }
-}
+        console.log('error', error);
+    };
+};
 
-postTest('/add', {temp:'68',feeling: 'good', date:'today' })
+postTest('/add', {temp:'68',feeling: 'good', date:'today' });
 
 
 /* GET Test */
 const getTest = async (url='')=>{
     const request = await fetch(url);
     try{
-        const allData = await request.json()
-        console.log(allData)
+        const allData = await request.json();
+        console.log(allData);
     }
     catch(error){
-        console.log("error", error)
+        console.log("error", error);
     }
 }
 
-getTest('/all')
+getTest('/all');
